e30cdbf29c6b649845fe51943beabf6256da180b
marwa <47919176+marowaadel@users.noreply.github.com>
2022-09-22 02:04:13 +0200

    Update test1.cpp

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
     int w=6;
+    //cin>>w;
+    if (w == 2)
+         cout << "no" <<endl;
+    else if(w%2==0)
+        cout << "yes" <<endl;
+    else
+        cout << "no" <<endl;
     return 0;
 }
